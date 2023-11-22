cb0a820e2e3c2cc4882de7640f858685235fffb3
Savecoders <pablopincay2003@gmail.com>
2023-11-21 22:01:06 -0500

    Refactor User DTO

--- src/models/user.py
+++ src/models/user.py
@@ -1,65 +1,94 @@
-class User:
-    def __init__(self, id, username, password, date_born, country, city, phone):
-        self._id = id
-        self._username = username
-        self._password = password
-        self._date_born = date_born
-        self._country = country
-        self._city = city
-        self._phone = phone
+# desing pattern: Data Transfer Object - DTO
+class Usuarios:
+    def __init__(self, id_usuario, nombres, contrasena, telefono, estado, email, image_perfil, pais, ciudad, fecha_nacimiento):
+        self._id = id_usuario
+        self._nombres = nombres
+        self._contrasena = contrasena
+        self._telefono = telefono
+        self._estado = estado
+        self._email = email
+        self._image_perfil = image_perfil
+        self._pais = pais
+        self._ciudad = ciudad
+        self._fecha_nacimiento = fecha_nacimiento
+
+    def __str__(self):
+        return f'{self._id}, {self._nombres}, {self._contrasena}, {self._telefono}, {self._estado}, {self._email}, {self._image_perfil}, {self._pais}, {self._ciudad}, {self._fecha_nacimiento}'
+
+    # GETTERS AND SETTERS
 
     @property
-    def id(self):
+    def id_usuario(self):
         return self._id
 
-    @id.setter
-    def id(self, id):
-        self._id = id
+    @property
+    def nombres(self):
+        return self._nombres
+
+    @nombres.setter
+    def nombres(self, nombres):
+        self._nombres = nombres
 
     @property
-    def username(self):
-        return self._username
+    def contrasena(self):
+        return self._contrasena
 
-    @username.setter
-    def username(self, username):
-        self._username = username
-
-    @property
-    def password(self):
-        return self._password
-
-    @password.setter
-    def password(self, password):
-        self._password = password
+    @contrasena.setter
+    def contrasena(self, contrasena):
+        self._contrasena = contrasena
 
     @property
-    def date_born(self):
-        return self._date_born
+    def telefono(self):
+        return self._telefono
 
-    @date_born.setter
-    def date_born(self, date_born):
-        self._date_born = date_born
-
-    @property
-    def country(self):
-        return self._country
-
-    @country.setter
-    def country(self, country):
-        self._country = country
+    @telefono.setter
+    def telefono(self, telefono):
+        self._telefono = telefono
 
     @property
-    def city(self):
-        return self._city
+    def estado(self):
+        return self._estado
 
-    @city.setter
-    def city(self, city):
-        self._city = city
+    @estado.setter
+    def estado(self, estado):
+        self._estado = estado
 
     @property
-    def phone(self):
-        return self._phone
+    def email(self):
+        return self._email
 
-    @phone.setter
-    def phone(self, phone):
-        self._phone = phone
+    @email.setter
+    def email(self, email):
+        self._email = email
+
+    @property
+    def image_perfil(self):
+        return self._image_perfil
+
+    @image_perfil.setter
+    def image_perfil(self, image_perfil):
+        self._image_perfil = image_perfil
+
+    @property
+    def pais(self):
+        return self._pais
+
+    @pais.setter
+    def pais(self, pais):
+        self._pais = pais
+
+    @property
+    def ciudad(self):
+        return self._ciudad
+
+    @ciudad.setter
+    def ciudad(self, ciudad):
+        self._ciudad = ciudad
+
+    @property
+    def fecha_nacimiento(self):
+        return self._fecha_nacimiento
+
+    @fecha_nacimiento.setter
+    def fecha_nacimiento(self, fecha_nacimiento):
+        self._fecha_nacimiento = fecha_nacimiento
