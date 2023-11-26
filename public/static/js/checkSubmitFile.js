4b056ce4eadb6215558edeed4d5c8ba0542286e5
Savecoders <pablopincay2003@gmail.com>
2023-11-26 18:20:03 -0500

    onclick image input icon

--- public/static/js/checkSubmitFile.js
+++ public/static/js/checkSubmitFile.js
@@ -1,18 +1,23 @@
-
+// selectors
 const $ = document.querySelector.bind(document);
+const inputFile = $(".file-upload-input");
 
 const checkSubmitFile = () => {
-    // label upload image 
-    const label = $('.file-upload-label');
-    // input image
-    const inputFile = $('#profile');
+  // label upload image
+  const label = $(".file-upload-label");
 
-    // check if file/image is selected
-    if (inputFile && inputFile.files.length) {
-        label.innerHTML = "Foto de Perfil seleccionada";
-        label.classList.add('file-upload-selected');
-    } else {
-        label.innerHTML = "Seleccionar Foto de Perfil";
-        label.classList.remove('file-upload-selected'); 
-    }
-}
+  // check if file/image is selected
+  if (inputFile && inputFile.files.length) {
+    label.innerHTML = "Foto de Perfil seleccionada";
+    console.log(inputFile.files.length);
+    label.classList.add("file-upload-selected");
+  } else {
+    label.innerHTML = "Seleccionar Foto de Perfil";
+    label.classList.remove("file-upload-selected");
+  }
+};
+
+const plusIcon = $(".image-plus__icon");
+plusIcon.addEventListener("click", () => {
+  inputFile.click();
+});
