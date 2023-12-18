264a623d83f6cb469b3d7b78f389801c64a4163e
Savecoders <pablopincay2003@gmail.com>
2023-12-18 11:29:32 -0500

    Add Donacion model class and Donacion schema

--- src/models/donacion.py
+++ src/models/donacion.py
@@ -1,3 +1,71 @@
+import re
+# uuid: Universally Unique Identifier
+import hashlib
+import uuid
+
 
 class Donacion:
-    pass
+
+    def __init__(self, id_donacion=None, monto=None, fecha_donacion=None, id_usuario=None, id_proyecto=None) -> None:
+        self.__id_donacion = id_donacion
+        self.__monto = monto
+        self.__fecha_donacion = fecha_donacion
+        self.__id_usuario = id_usuario
+        self.__id_proyecto = id_proyecto
+
+    @property
+    def id_donacion(self):
+        return self.__id_donacion
+
+    @id_donacion.setter
+    def id_donacion(self, id_donacion):
+        self.__id_donacion = id_donacion
+
+    @property
+    def monto(self):
+        return self.__monto
+
+    @monto.setter
+    def monto(self, monto):
+
+        if str(monto).isdigit() == False:
+            raise ValueError("El monto debe ser un número")
+
+        if float(monto) > 0:
+            self.__monto = float(monto)
+        else:
+            raise ValueError("El monto no puede ser menor o igual a 0")
+
+    @property
+    def fecha_donacion(self):
+        return self.__fecha_donacion
+
+    @fecha_donacion.setter
+    def fecha_donacion(self, fecha_donacion):
+
+        if fecha_donacion == "":
+            raise ValueError("La fecha no puede estar vacía")
+
+        self.__fecha_donacion = fecha_donacion
+
+    @property
+    def id_usuario(self):
+        return self.__id_usuario
+
+    @id_usuario.setter
+    def id_usuario(self, id_usuario):
+        self.__id_usuario = id_usuario
+
+    @property
+    def id_proyecto(self):
+        return self.__id_proyecto
+
+    @id_proyecto.setter
+    def id_proyecto(self, id_proyecto):
+        self.__id_proyecto = id_proyecto
+
+    def generateHashId(self):
+        self.__id_donacion = hashlib.sha256(uuid.uuid4().bytes).hexdigest()
+
+    def inserdao(self):
+        return (self.__id_donacion, self.__monto, self.__fecha_donacion, self.__id_usuario, self.__id_proyecto)
--- src/schema/donacion_dao.py
+++ src/schema/donacion_dao.py
@@ -1,3 +1,102 @@
+from oracledb import Connection, Cursor
+from src.models import Donacion
+
 
 class DonacionDao:
-    pass
+
+    def __init__(self, conn: Connection, cursor: Cursor) -> None:
+        self.__conn = conn
+        self.__cursor = cursor
+
+    def insert(self, donacion: Donacion) -> None:
+
+        if donacion is None:
+            raise ValueError("The Error al insertar la donacion")
+
+        sql = """
+        INSERT INTO DONACION (IDDONACION, MONTO, FECHADONACION, IDUSUARIO, IDPROYECTO)
+        VALUES (:1, :2, :3, :4, :5)
+        """
+
+        self.__cursor.execute(sql, donacion.inserdao())
+        self.__conn.commit()
+
+        sql = """
+        INSERT INTO PROYECTOS_DONACION (IDPROYECTO, IDDONACION)
+        VALUES (:1, :2)
+        """
+
+        values = (donacion.id_proyecto, donacion.id_donacion)
+
+        self.__cursor.execute(sql, values)
+
+        self.__conn.commit()
+
+    def get_all_donations_by_user(self, id_user: str) -> list[Donacion]:
+
+        sql = """
+        SELECT * FROM DONACION WHERE IDUSUARIO = :1
+        """
+
+        self.__cursor.execute(sql, (id_user,))
+
+        data = self.__cursor.fetchall()
+
+        if not data:
+            return None
+
+        donaciones = []
+
+        for row in data:
+
+            donacion = Donacion(row[0], row[1], row[2], row[3], row[4])
+
+            donaciones.append(donacion)
+
+        return donaciones
+
+    def get_all_donations_by_project(self, id_project: str) -> list[Donacion]:
+
+        sql = """
+        SELECT * FROM DONACION WHERE IDPROYECTO = :1
+        """
+
+        self.__cursor.execute(sql, (id_project,))
+
+        data = self.__cursor.fetchall()
+
+        if not data:
+            return None
+
+        donaciones = []
+
+        for row in data:
+
+            donacion = Donacion(row[0], row[1], row[2], row[3], row[4])
+
+            donaciones.append(donacion)
+
+        return donaciones
+
+    def get_all_donations(self) -> list[Donacion]:
+
+        sql = """
+        SELECT * FROM DONACION
+        """
+
+        self.__cursor.execute(sql)
+
+        data = self.__cursor.fetchall()
+
+        if not data:
+            return None
+
+        donaciones = []
+
+        for row in data:
+
+            donacion = Donacion(row[0], row[1], row[2], row[3], row[4])
+
+            donaciones.append(donacion)
+
+        return donaciones
